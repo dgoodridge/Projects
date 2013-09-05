81eebfc051daab78d50d66df94ebbb1ca59fa6ce
dgoodridge <danielgoodridge85@gmail.com>
2013-09-05 19:11:53 -0400

    Where am I?
    Removed WF from solution, JQuery error on edit page

---
+++ CthTest/CthTest/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="c5a0a367-0764-4695-89f7-8ed2d3af6f43" featureId="c5a0a367-0764-4695-89f7-8ed2d3af6f43" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="CthTest Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="d133c062-c403-44f8-b6b8-60658eefcccf" />
+    <projectItemReference itemId="c429bba0-d79c-4fc4-9fc6-8e3dc397709d" />
+    <projectItemReference itemId="855edaae-b4b9-4089-a306-dffe4d8e101e" />
+    <projectItemReference itemId="61962a8f-55a5-4eaa-bf6b-5d6f232f492b" />
+  </projectItems>
+</feature>
---
+++ SafetyFirstWF/SafetyFirstWF/SafetyFirstSqtWF/SafetyFirstSqtWF.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Drawing;
+using System.Linq;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Workflow;
+using Microsoft.SharePoint.WorkflowActions;
+
+namespace SafetyFirstWF.SafetyFirstSqtWF
+{
+    public sealed partial class SafetyFirstSqtWF : SequentialWorkflowActivity
+    {
+        public SafetyFirstSqtWF()
+        {
+            InitializeComponent();
+        }
+
+        public Guid workflowId = default(System.Guid);
+        public SPWorkflowActivationProperties workflowProperties = new SPWorkflowActivationProperties();
+
+        private void codeActivity1_ExecuteCode(object sender, EventArgs e)
+        {
+            using (SPWeb oWeb = workflowProperties.Web)
+            {
+                SPListItem currentItem = workflowProperties.Item;
+
+                string resolutionStatus = (string)currentItem["Resolution Status"];
+
+            }
+        }
+    }
+}
